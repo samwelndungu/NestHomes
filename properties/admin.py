8116d9ebae7bb639d2892036bd60c790dbfe5246
samwelndungu <ndungusamwel460@gmail.com>
2025-06-25 23:37:18 +0300

    Fixed backend folder structure and added files

--- properties/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Property
-
-@admin.register(Property)
-class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'location')  # Columns shown in admin list
